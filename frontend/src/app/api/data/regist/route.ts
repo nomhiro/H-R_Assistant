d1bb4df9abfc8f73781592245275bb3900f4afb1
Hiroki Nomura <nomhiro1204@gmail.com>
2025-01-12 16:29:53 +0900

    fix: フォルダパスの存在確認を追加し、エラーメッセージを改善

--- frontend/src/app/api/data/regist/route.ts
+++ frontend/src/app/api/data/regist/route.ts
@@ -16,7 +16,7 @@ export const POST = async (req: NextRequest) => {
       folderPath = formData.get("folderPath") as string;
 
       // ファイルがアップロードされているか確認
-      if (file) {
+      if (file && folderPath) {
         console.log(" 🚀ファイル（PDF、画像ファイル）を登録します");
         console.log(" 🚀フォルダパス: ", folderPath);
         console.log(" 🚀ファイル名: ", (file as File).name);
@@ -27,7 +27,7 @@ export const POST = async (req: NextRequest) => {
           console.log(" ❌フォルダパスが無効です。");
         }
       } else {
-        console.log(" ❌ファイルがアップロードされていません。");
+        console.log(" ❌ファイルまたはフォルダパスがアップロードされていません。");
       }
     } else if (contentType.includes("application/json")) {
       const json = await req.json();

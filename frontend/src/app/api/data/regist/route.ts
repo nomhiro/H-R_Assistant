3911ab8fa6d34d8e406e527fa94249dbbe033e08
Hiroki Nomura <nomhiro1204@gmail.com>
2025-01-12 20:27:59 +0900

    fix: アップロード機能のコードをコメントアウト

--- frontend/src/app/api/data/regist/route.ts
+++ frontend/src/app/api/data/regist/route.ts
@@ -6,38 +6,40 @@ export const POST = async (req: NextRequest) => {
   try {
     const contentType = req.headers.get("content-type") || "";
 
-    if (contentType.includes("multipart/form-data")) {
-      // const formData = await req.formData();
-      // const file = formData.get("file");
-      // const folderPath = formData.get("folderPath");
+    console.log("contentType:", contentType);
 
-      // console.log("formData:", formData);
-      // console.log("file:", file);
-      // console.log("folderPath:", folderPath);
+    // if (contentType.includes("multipart/form-data")) {
+    // const formData = await req.formData();
+    // const file = formData.get("file");
+    // const folderPath = formData.get("folderPath");
 
-      //   if (file && typeof folderPath === 'string') {
-      //     console.log("🚀ファイル（PDF、画像ファイル）を登録します");
-      //     console.log("🚀フォルダパス: ", folderPath);
-      //     console.log("🚀ファイル名: ", (file as File).name);
+    // console.log("formData:", formData);
+    // console.log("file:", file);
+    // console.log("folderPath:", folderPath);
 
-      //     if (file instanceof File) {
-      //       console.log("🚀ファイルはFileインスタンスです");
-      //       try {
-      //         await uploadFileToFolder(folderPath, file);
-      //       } catch (error) {
-      //         console.error(`❌ ファイルのアップロード中にエラーが発生しました: ${error}`);
-      //       }
-      //     } else {
-      //       console.log("❌ ファイルがFileインスタンスではありません");
-      //     }
-      //   } else {
-      //     console.log("❌ ファイルまたはフォルダパスがアップロードされていません。");
-      //   }
-      // } else if (contentType.includes("application/json")) {
-      //   const json = await req.json();
-      //   const text = json.text;
-      //   console.log("🚀 テキストデータを登録します。text: ", text);
-    }
+    //   if (file && typeof folderPath === 'string') {
+    //     console.log("🚀ファイル（PDF、画像ファイル）を登録します");
+    //     console.log("🚀フォルダパス: ", folderPath);
+    //     console.log("🚀ファイル名: ", (file as File).name);
+
+    //     if (file instanceof File) {
+    //       console.log("🚀ファイルはFileインスタンスです");
+    //       try {
+    //         await uploadFileToFolder(folderPath, file);
+    //       } catch (error) {
+    //         console.error(`❌ ファイルのアップロード中にエラーが発生しました: ${error}`);
+    //       }
+    //     } else {
+    //       console.log("❌ ファイルがFileインスタンスではありません");
+    //     }
+    //   } else {
+    //     console.log("❌ ファイルまたはフォルダパスがアップロードされていません。");
+    //   }
+    // } else if (contentType.includes("application/json")) {
+    //   const json = await req.json();
+    //   const text = json.text;
+    //   console.log("🚀 テキストデータを登録します。text: ", text);
+    // }
 
     return NextResponse.json({ message: "📄 データが正常に登録されました" }, { status: 200 });
   } catch (error) {

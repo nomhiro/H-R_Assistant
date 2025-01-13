bbaf55a6a0f03be635f59a6162564b666a335977
Hiroki Nomura <nomhiro1204@gmail.com>
2025-01-13 20:55:36 +0900

    feat: ルートをリネームし、推論APIの実装を更新

--- frontend/src/util/generate.ts
+++ frontend/src/util/generate.ts
@@ -1,48 +1,79 @@
-import { getEmbedding, getChatCompletions } from './openai'; 
+import { getEmbedding, getChatCompletions, getQueryJson } from './openai';
 import { getItemsByVector } from './cosmos';
 import { getBase64File } from './blob';
+import { Query } from '../models/models';
+import { CosmosItem } from "../models/models";
 
-export const getOnYourData = async (message: string): Promise<any[]> => {
+let VECTOR_SCORE = parseFloat(process.env.VECTOR_SCORE!);
+
+export const getInferenceRAG = async (message: string): Promise<string> => {
   return new Promise(async (resolve, reject) => {
-    // ベクトル化
-    console.log('🚀Get embedding from Azure OpenAI.');
-    const embeddedMessage = await getEmbedding(message);
+    try {
+      // ユーザメッセージを、Keywordと検索用文章に変換
+      console.log('🚀Convert message to keyword and search sentence.');
+      const query: Query = await getQueryJson(message);
 
-    // CosmosDBでベクトル検索
-    console.log('🚀Search vector from Azure CosmosDB.');
-    const cosmosItems = await getItemsByVector(embeddedMessage);
+      // ベクトル化
+      console.log('🚀Get embedding from Azure OpenAI.');
+      const embeddedMessage = await getEmbedding(query.search_text);
 
-    // CosmosDBのベクトル検索で精度が出ない場合は、AISearchを使ったセマンティック検索などを使う　※仮実装
-    // const searchResults = await searchHybrid(message, embeddedMessage);
+      let cosmosItems: CosmosItem[] = [];
+      let attempts = 0;
 
-    // systemMessageにRAGの情報を追加
-    console.log('🚀Create system message and image_content.');
-    let systemMessage = 'あなたが持っている知識は使ってはいけません。 "検索結果" と画像の情報のみを使い回答しなさい。わからない場合は「分かりません。」と回答しなさい。';
-    systemMessage += '# 検索結果\n'
-    let images: string[] = [];
-    let responseImageUrl: string = "";
-    for (const result of cosmosItems) {
-      // ループ番号を追加
-      systemMessage += '## ' + (cosmosItems.indexOf(result) + 1) + '\n' + result.content + '\n\n';;
-      // 画像の取得
-      if (result.is_contain_image === true) {
-        const image = await getBase64File(result.image_blob_path);
-        images.push(image);
+      while (cosmosItems.length === 0 && attempts < 3) {
+        // CosmosDBでベクトル検索
+        console.log(`🚀Search vector from Azure CosmosDB with VECTOR_SCORE: ${VECTOR_SCORE}.`);
+        cosmosItems = await getItemsByVector(embeddedMessage, VECTOR_SCORE.toString());
 
-        responseImageUrl += result.image_blob_path + ': ' + result.SimilarityScore + '  \n';
+        if (cosmosItems.length === 0) {
+          VECTOR_SCORE -= 0.03;
+          attempts++;
+          console.log(`🚀No items found. Retrying with VECTOR_SCORE: ${VECTOR_SCORE}. Attempt: ${attempts}`);
+        }
       }
-    }
 
-    // OpenAI へのリクエスト
-    const result = await getChatCompletions(systemMessage, message, images);
-    let aiMessage = result[0].message.content;
-    // もしimageがあればaiMessageとresponseImageUrlを改行でつなぐ
-    if (responseImageUrl !== "") {
-      aiMessage += '  \n  \n' + "◆参考画像";
-      aiMessage += '  \n' + responseImageUrl;
-    }
+      if (cosmosItems.length === 0) {
+        console.log('🚀No items found after 3 attempts.');
+      }
 
-    resolve(aiMessage);
+      // systemMessageにRAGの情報を追加
+      console.log('🚀Create system message and image_content.');
+      let systemMessage = 'あなたが持っている知識は使ってはいけません。 "検索結果" と画像の情報のみを使い回答しなさい。わからない場合は「分かりません。」と回答しなさい。';
+      systemMessage += '# 検索結果\n'
+      let images: string[] = [];
+      let responseImageUrl: string = "";
+      for (const result of cosmosItems) {
+        // ループ番号を追加
+        systemMessage += '## ' + (cosmosItems.indexOf(result) + 1) + '\n' + result.content + '\n\n';;
+        // 画像の取得
+        if (result.is_contain_image === true) {
+          const image = await getBase64File(result.image_blob_path);
+          images.push(image);
+
+          responseImageUrl += result.image_blob_path + ': ' + result.SimilarityScore + '  \n';
+        }
+      }
+
+      // OpenAI へのリクエスト
+      const result = await getChatCompletions(systemMessage, message, images);
+      let aiMessage = result[0].message.content;
+      // 検索結果(cosmosItem)があればaiMessageと改行でつなぐ。スコアもつける
+      let displaySearchedDoc = '';
+      if (cosmosItems.length > 0) {
+        displaySearchedDoc = '---\n### 検索結果\n';
+        for (const item of cosmosItems) {
+          displaySearchedDoc += (cosmosItems.indexOf(item) + 1) + '. ' + item.file_name + ' : ' + item.SimilarityScore + '\n';
+        }
+      }
+
+      const resultMessage = aiMessage + '\n\n' + displaySearchedDoc;
+
+      resolve(resultMessage);
+    }
+    catch (error: any) {
+      console.error('  ❌Error in getOnYourData:', error);
+      reject(error);
+    }
 
   })
 }
